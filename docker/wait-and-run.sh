734d6dbb7d604eedd4d062bd53520d8194dadfcc
halkano liban <halkanosalad6@email.com>
2026-04-13 13:43:14 +0300

    V2

--- docker/wait-and-run.sh
+++ docker/wait-and-run.sh
@@ -3,11 +3,16 @@ set -e
 
 host="${DB_HOST:-mysql}"
 port="${DB_PORT:-3306}"
+app_port="${PORT:-8080}"
 
 echo "Waiting for MySQL at ${host}:${port}..."
 until bash -lc ">/dev/tcp/${host}/${port}" 2>/dev/null; do
   sleep 2
 done
 echo "MySQL is ready."
+
+if [ -f /usr/local/tomcat/conf/server.xml ]; then
+  sed -i "0,/port=\"8080\"/s/port=\"8080\"/port=\"${app_port}\"/" /usr/local/tomcat/conf/server.xml
+fi
 
 exec catalina.sh run
