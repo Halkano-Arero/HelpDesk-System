de392bdcdb891905ed0ce1b1ce33bbfaed5cb6bb
halkano liban <halkanosalad6@email.com>
2026-04-13 14:05:28 +0300

    hha

--- src/main/java/com/helpdesk/listener/ApplicationLifecycleListener.java
+++ src/main/java/com/helpdesk/listener/ApplicationLifecycleListener.java
@@ -1,6 +1,8 @@
 package com.helpdesk.listener;
 
 import com.helpdesk.db.DBConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -8,9 +10,15 @@ import javax.servlet.annotation.WebListener;
 @WebListener
 public class ApplicationLifecycleListener implements ServletContextListener {
 
+    private static final Logger LOGGER = Logger.getLogger(ApplicationLifecycleListener.class.getName());
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        DBConnection.ensureSchema();
+        try {
+            DBConnection.ensureSchema();
+        } catch (RuntimeException ex) {
+            LOGGER.log(Level.WARNING, "Database is not reachable during startup. The web app will still start, but DB-backed pages may fail until the connection is fixed.", ex);
+        }
     }
 
     @Override
